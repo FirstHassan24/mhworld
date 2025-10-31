bfd4921dfca369a72ea4896cf53c167ae3196ee8
Akilou24 <61370309+FirstHassan24@users.noreply.github.com>
2025-10-31 16:38:05 -0400

    update:added fields for my models.py and set them up as optional or required.

--- monsterhunter/models.py
+++ monsterhunter/models.py
@@ -1,4 +1,16 @@
 from django.db import models
 
-#what do i want to store for each monster?:
 # Create your models here.
+#what do i want to store for each monster?:
+class Monster(models.Model):
+    #every monster name must be unique:
+    name = models.CharField(unique=True, max_length=50),
+    #what type the monster is (brute wyvern,eldar dragon,flying wyvern etc):
+    type = models.CharField(max_length=100,null=True,blank=True),
+    #display the type of element it will use
+    element = models,models.CharField(max_length=100,null=True,blank=True),
+    #add a small description for each monster explaining their biography:
+    description = models.CharField(max_length=500,null=True,blank=True)
+    #add an image for each monster:
+    image_url = models.UrlField()
+
