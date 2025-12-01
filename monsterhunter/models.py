b9dd5281399a637da5097e7cc764a1678ef747fc
Akilou24 <61370309+FirstHassan24@users.noreply.github.com>
2025-12-01 16:06:31 -0500

    Update:made an element model which creates an element table. and now im thinking that for each row of the monster will be the elements like ice,fire,dragon etc and the columns will be the monsters with those elements

--- monsterhunter/models.py
+++ monsterhunter/models.py
@@ -12,8 +12,38 @@ class Monster(models.Model):
     #add a small description for each monster explaining their biography:
     description = models.CharField(max_length=500,null=True,blank=True)
     #add an image for each monster:
-    image_url = models.URLField()
+    image_url = models.URLField(null=True,blank=True)
     #make django show me the name when printing the monster:
     def __str__(self):
         return self.name
+#TODO: make elements into a seperate table and make an association:
+class Element(models.Model):
+    name = models.CharField(unique=True)
+
     
+#create an item table:
+class Item(models.Model):
+    #what fields do i need from items?
+    name = models.CharField(unique=True,max_length=50)
+    description = models.TextField(null=True,blank=True)
+    rarity = models.IntegerField()
+    value = models.IntegerField()  
+    #shows me the name of the table when i print it:
+    def __str__(self):
+        return self.name 
+
+#create a many to many relationship between monster and item:
+class MonsterItemDrop(models.Model):
+    #which monster is this drop for?
+    monster = models.ForeignKey(Monster,on_delete=models.CASCADE)
+    #which item does it drop?:
+    item = models.ForeignKey(Item,on_delete=models.CASCADE)
+    #how is it obtained?
+    #TODO make a choice field so the user can only choose between carving and capture
+    method = models.CharField(max_length=100)
+    #what are the drop chance:
+    chance = models.IntegerField()
+    
+
+#TODO: add a text field for item, make it so the user can type whatever add it to a list of items,find the ones that existes and then associate them and create the ones that dont exist and asscociate them.
+#TODO:make it so when the user types it shows them autocomplete of whats in the api, 
